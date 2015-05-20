257bf36519e6521042fe5b9d97cae66b7dcc9ba1
Dustin Kraft <kraftdr@gmail.com>
2015-05-20 11:05:50 -0600

    EF Changes

---
+++ eCommerce/eCommerce/Migrations/201505201648430_initial.cs
@@ -0,0 +1,44 @@
+namespace eCommerce.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductImages",
+                c => new
+                    {
+                        ProductImageId = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        ImageUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.ProductImageId)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        MSRP = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ProductImages", "ProductId", "dbo.Products");
+            DropIndex("dbo.ProductImages", new[] { "ProductId" });
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductImages");
+        }
+    }
+}
---
+++ eCommerce/eCommerce/Models/ProductDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity; 
+
+namespace eCommerce.Models
+{
+    public class ProductDataContext: DbContext
+    {
+        //Add the "tables"
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductImage> ProductImages { get; set; }
+
+        //Create a constructor.  The name=ProductDatabase bit points to the ProductDatabase connection string in the Web.Config
+        public ProductDataContext()
+            : base("name=DefaultConnection")
+        {
+
+        }
+    }
+}
